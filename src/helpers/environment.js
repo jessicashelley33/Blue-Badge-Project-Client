baeaf1b134b803eba2454c37c9ec6aa08124e421
jessicashelley33 <85132275+jessicashelley33@users.noreply.github.com>
2021-08-04 14:47:53 -0400

    Updated URL

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,12 +1,12 @@
 let APIURL = '';
 
 switch (window.location.hostname) {
-    case `localhost` || `127.0.0.1`:
-        APIURL = `http://localhost:5000`;
+    case 'localhost' || '127.0.0.1':
+        APIURL = 'http://localhost:5000';
         break;
 
         case 'travel-agency-client-2021':
-            APIURL = `https://travel-agency-client-2021.herokuapp.com`
+            APIURL = 'https://travel-app-blue-badge.herokuapp.com'
 }
 
 export default APIURL
